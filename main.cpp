05eb70f7bc0bf2dbe308f3ca19a72193f879e761
大大狐狸cyberfox_meow <bigbigfox@office.qianbai.tw>
2022-09-29 18:49:14 -0500

    加入外部输入

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ using namespace std;
 
 // Goldbach's conjecture using MIRACL
 
-int main() {
-
+int main(int argc, char** argv) {
 
     miracl* mip = mirsys(5000, 10);  /* initialise system to base 10, 5000 digits per "big" */
 
@@ -26,14 +25,17 @@ int main() {
     big bigzero;
     bigzero=mirvar(0);
 
-    irand((unsigned)time(NULL));
+    //irand((unsigned)time(NULL));
 
-    bigbits(1500, composite_number);
+    //bigbits(1800, composite_number);
+    //77785013981551582343293798510443924959819057315170007596898940617969596087533115026951499894039902248846791609082584911446550364291013377086145727196953287853703181523044693184006168499541635734974518338177127986068424623677884947462862567494290279550993930991636393774544224102456139692653784144946384674721399141364811253440183811656861558720345735944803400260533998259914078019082187647176761313535767485202646237839574347186337095641076426343761905828352269566830703817878713983333711383742746209161506947321366421127198006811720502000160
+
+    cinstr(composite_number,argv[1]);
 
     multiply(composite_number, mirvar(2),composite_number);
 
-    cotnum(composite_number,stdout);
-    printf(" =\n");
+    //cotnum(composite_number,stdout);
+    //printf(" =\n");
 
     int result_count=0;
 
@@ -62,10 +64,10 @@ int main() {
 
         if(isprime_result==true){
             cotnum(number1,stdout);
-            printf(" + ");
+            //printf(" + ");
             cotnum(number2,stdout);
             result_count++;
-          //  if(result_count==10)break;
+            if(result_count==10)break;
         }
         else{
             continue;
